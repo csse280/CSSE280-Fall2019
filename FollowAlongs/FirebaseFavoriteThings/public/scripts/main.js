89e067635ad65c3f4b59c4eac3f20be7d4fba897
David Fisher <fisherds@rose-hulman.edu>
2019-10-01 15:44:24 -0400

    listening for data changes

--- FollowAlongs/FirebaseFavoriteThings/public/scripts/main.js
+++ FollowAlongs/FirebaseFavoriteThings/public/scripts/main.js
@@ -9,24 +9,48 @@
 var rh = rh || {};
 
 /** globals */
-rh.variableName = "";
+rh.docRef = null;
+rh.FB_KEY_COLOR = "color";
+rh.FB_KEY_NUMBER = "number";
+rh.FB_KEY_IS_TABS = "isTabs";
+
 
 /** function and class syntax examples */
-rh.functionName = function () {
-	/** function body */
+rh.beginListening = function () {
+	
+	rh.docRef.onSnapshot(function(doc) {
+
+		if (doc.exists) {
+			console.log("Document data:", doc.data());
+			const color = doc.get(rh.FB_KEY_COLOR);
+			const number = doc.get(rh.FB_KEY_NUMBER);
+			const isTabs = doc.get(rh.FB_KEY_IS_TABS);
+
+			console.log("Received: ", color, typeof(color));
+			console.log("Received: ", number, typeof(number));
+			console.log("Received: ", isTabs, typeof(isTabs));
+
+			$("#favorite-color").html(color);
+			$("#favorite-number").html(number);
+			$("#favorite-indent").html(isTabs ? "Tabs" : "Spaces");
+
+		} else {
+			// doc.data() will be undefined in this case
+			console.log("No such document!");
+			$("#favorite-color").html("unknown");
+			$("#favorite-number").html("unknown");
+			$("#favorite-indent").html("unknown");
+		}
+
+        
+    });
+
 };
-
-rh.ClassName = class {
-	/** constructor */
-	constructor() {
-
-	}
-	methodName() {
-
-	}
-}
 
 /* Main */
 $(document).ready(() => {
-    console.log("Ready");
+	console.log("Ready");
+	rh.docRef = firebase.firestore().collection("FavoriteThings").doc("mine");
+	rh.beginListening();
+
 });
