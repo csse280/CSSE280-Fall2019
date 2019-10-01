fbd11bec1e71c050d9176ec2f05a52a9ae6ac7e4
David Fisher <fisherds@rose-hulman.edu>
2019-10-01 14:22:43 -0400

    copy in a new template

---
+++ FollowAlongs/FirebaseFavoriteThings/public/scripts/main.js
@@ -0,0 +1,32 @@
+/**
+ * @fileoverview
+ * Provides interactions for all pages in the UI.
+ *
+ * @author 
+ */
+
+/** namespace. */
+var rh = rh || {};
+
+/** globals */
+rh.variableName = "";
+
+/** function and class syntax examples */
+rh.functionName = function () {
+	/** function body */
+};
+
+rh.ClassName = class {
+	/** constructor */
+	constructor() {
+
+	}
+	methodName() {
+
+	}
+}
+
+/* Main */
+$(document).ready(() => {
+    console.log("Ready");
+});
