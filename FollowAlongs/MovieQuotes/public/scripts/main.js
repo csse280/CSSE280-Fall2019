549f9311544ecfb68b3ff13f7089b88c372f706f
David Fisher <fisherds@rose-hulman.edu>
2019-10-07 15:40:46 -0400

    add a movie quote

--- FollowAlongs/MovieQuotes/public/scripts/main.js
+++ FollowAlongs/MovieQuotes/public/scripts/main.js
@@ -40,9 +40,17 @@ rh.FbMovieQuotesManager = class {
 	stopListening() {
 		this._unsubscribe();
 	}
-	
-	add(quote, movie) {
 
+	add(quote, movie) {
+		this._ref.add({
+			[rh.KEY_QUOTE]: quote,
+			[rh.KEY_MOVIE]: movie,
+			[rh.KEY_LAST_TOUCHED]: firebase.firestore.Timestamp.now()
+		}).then((docRef) => {
+			console.log("Document has been added with id", docRef.id);
+		}).catch((error) => {
+			console.log("There was an error adding the document", error);
+		});
 	}
 	update(id, quote, movie) {
 
@@ -67,7 +75,16 @@ rh.FbMovieQuotesManager = class {
 rh.ListPageController = class {
 	constructor() {
 		rh.fbMovieQuotesManager.beginListening(this.updateView.bind(this));
+
+		// TODO: On Monday improve the form shown stuff.
+
+		$("#submitAddQuote").click((event) => {
+			const quote = $("#inputQuote").val();
+			const movie = $("#inputMovie").val();
+			rh.fbMovieQuotesManager.add(quote, movie);
+		})
 	}
+	
 	updateView() {
 		console.log("The model object has changed.  I need to use it!", this);
 	}
