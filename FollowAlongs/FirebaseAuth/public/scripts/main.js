9a628b2875d814e092b35d94172d14151ad4aec8
David Fisher <fisherds@rose-hulman.edu>
2019-10-22 14:38:45 -0400

    made a placehold sign out button

--- FollowAlongs/FirebaseAuth/public/scripts/main.js
+++ FollowAlongs/FirebaseAuth/public/scripts/main.js
@@ -8,8 +8,24 @@
 /** namespace. */
 var rh = rh || {};
 
-rh.beginAuthListening = function() {
-	// TODO: Start here on Tuesday
+rh.beginAuthListening = function () {
+	firebase.auth().onAuthStateChanged(function (user) {
+		if (user) {
+			// User is signed in.
+			var displayName = user.displayName;
+			var email = user.email;
+			var emailVerified = user.emailVerified;
+			var photoURL = user.photoURL;
+			var isAnonymous = user.isAnonymous;
+			var uid = user.uid;
+			var providerData = user.providerData;
+
+			console.log("A user IS signed in.  Uid = ", user.uid);
+		} else {
+			// User is signed out.
+			console.log("There is no user.  Nobody is signed in.");
+		}
+	});
 }
 
 rh.enableEmailPassword = function () {
@@ -17,6 +33,23 @@ rh.enableEmailPassword = function () {
 	const password = new mdc.textField.MDCTextField(document.querySelector('.password'));
 	new mdc.ripple.MDCRipple(document.querySelector('#createAccount'));
 	new mdc.ripple.MDCRipple(document.querySelector('#login'));
+
+	$("#createAccount").click((event) => {
+		const emailValue = $("#email-input").val();
+		const passwordValue = $("#password-input").val();
+		console.log("Create a new user", emailValue, passwordValue);
+		firebase.auth().createUserWithEmailAndPassword(emailValue, passwordValue).catch(function (error) {
+			// CONSIDER: In a real tell the user what is wrong.
+			console.log(`Error ${error.code}: ${error.message}`);
+		});
+
+
+
+	});
+	$("#login").click((event) => {
+		console.log("TODO: Log in an existing user");
+	});
+
 };
 
 /* Main */
