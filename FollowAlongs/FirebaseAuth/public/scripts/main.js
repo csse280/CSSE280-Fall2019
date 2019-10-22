93ed90d03c64279b68748783d0e29688faa1aee3
David Fisher <fisherds@rose-hulman.edu>
2019-10-22 15:32:12 -0400

    finished firebase auth app

--- FollowAlongs/FirebaseAuth/public/scripts/main.js
+++ FollowAlongs/FirebaseAuth/public/scripts/main.js
@@ -19,12 +19,15 @@ rh.beginAuthListening = function () {
 			$("#phoneNumber").html(`<b>phone #</b>: ${user.phoneNumber}`);
 			console.log(user.providerData);
 			console.log("A user IS signed in.  Uid = ", user.uid);
+			
+			$("#firebaseui-auth-container").hide();
 			$("#emailPassword").hide();
 			$("#userInfo").show();
 		} else {
 			// User is signed out.
 			console.log("There is no user.  Nobody is signed in.");
-			$("#emailPassword").show();
+			$("#emailPassword").hide();  // Turned off for now.
+			$("#firebaseui-auth-container").show();
 			$("#userInfo").hide();
 		}
 	});
@@ -57,6 +60,21 @@ rh.enableEmailPassword = function () {
 	});
 };
 
+rh.startFirebaseUi = function () {
+	// FirebaseUI config.
+	var uiConfig = {
+        signInSuccessUrl: '/',
+        signInOptions: [
+          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+          firebase.auth.EmailAuthProvider.PROVIDER_ID,
+          firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+          firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID,
+        ],
+      };
+      var ui = new firebaseui.auth.AuthUI(firebase.auth());
+      ui.start("#firebaseui-auth-container", uiConfig);
+}
+
 /* Main */
 $(document).ready(() => {
 	console.log("Ready");
@@ -66,6 +84,7 @@ $(document).ready(() => {
 
 	rh.enableEmailPassword();
 	rh.beginAuthListening();
+	rh.startFirebaseUi();
 
 	$("#signOut").click((event) => {
 		firebase.auth().signOut()
