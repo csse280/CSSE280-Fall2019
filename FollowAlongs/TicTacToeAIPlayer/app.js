79ba27367c1e70d8fec260e6fed6ec9ce1997baf
David Fisher <fisherds@rose-hulman.edu>
2019-11-12 14:52:57 -0500

    backend api complete

--- FollowAlongs/TicTacToeAIPlayer/app.js
+++ FollowAlongs/TicTacToeAIPlayer/app.js
@@ -3,6 +3,28 @@ var app = express();
 
 app.use(express.static("public"));
 
+function getOpenLocations(boardString) {
+  const openLocations = [];
+  for (let k = 0; k < boardString.length; k++) {
+    if (boardString.charAt(k) == '-') {
+      openLocations.push(k)
+    }
+  }
+  return openLocations;
+}
+
+app.get("/api/getmove/:board", (request, response) => {
+  const boardString = request.params.board;
+  const openLocations = getOpenLocations(boardString);
+  const moveSelected = openLocations[Math.floor(Math.random()*openLocations.length)];
+
+  //var item = items[Math.floor(Math.random()*items.length)];
+
+  // console.log(openLocations);
+  // console.log(moveSelected);
+  response.json({move: moveSelected});
+});
+
 app.listen(3000, (err) => {
   if (err) {
      console.log("Node Error!", err);
